36fd098ba99d11515595604f5e946f3a5f2a8741
BevanMkhabele <148946458+BevanMkhabele@users.noreply.github.com>
2023-11-04 13:28:34 +0200

    Update process_registration.php

--- process_registration.php
+++ process_registration.php
@@ -1,38 +1,37 @@
 <?php
-// Database connection details
-$host = "localhost"; // Use "localhost" for a database on the same server
-$dbname = "user_input"; // Updated to match your database name
-$username = "route"; // Updated to match your database username (route)
-$password = ""; // Database password is blank
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "bursaries_data";
 
-try {
-    // Create a new PDO (PHP Data Objects) instance for database connection
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Create a connection to the database
+$conn = new mysqli($servername, $username, $password, $database);
 
-    // Retrieve form data from POST request
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $idNumber = $_POST['idNumber'];
-    $phoneNumber = $_POST['phoneNumber'];
-
-    // SQL query to insert data into the "user_input" table
-    $sql = "INSERT INTO user_input (name, surname, idNumber, phoneNumber) VALUES (:name, :surname, :idNumber, :phoneNumber)";
-
-    // Prepare and execute the SQL statement with the provided data
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([
-        ':name' => $name,
-        ':surname' => $surname,
-        ':idNumber' => $idNumber,
-        ':phoneNumber' => $phoneNumber,
-    ]);
-
-    // Successful registration message
-    echo "Registration complete. You will be sent an SMS to confirm your application.";
-
-} catch (PDOException $e) {
-    // Handle database connection or query errors
-    echo "Error: " . $e->getMessage();
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Process form submission
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Retrieve user input from the form
+    $name = $_GET["name"];
+    $surname = $_GET["surname"];
+    $idNumber = $_GET["idNumber"];
+    $phoneNumber = $_GET["phoneNumber"];
+
+    // SQL query to insert data into the UserInformation table
+    $sql = "INSERT INTO UserInformation (name, surname, sa_id, phone_number) VALUES ('$name', '$surname', '$idNumber', '$phoneNumber')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful. Data has been stored in the database.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
 ?>
+
